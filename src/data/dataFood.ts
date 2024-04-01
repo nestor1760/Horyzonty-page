59424018a3b23193491c9c288ef670bb732b3585
Roman Nesterchuk <133716724+nestor1760@users.noreply.github.com>
2024-04-01 19:39:49 +0200

    Update dataFood.ts

--- src/data/dataFood.ts
+++ src/data/dataFood.ts
@@ -3,19 +3,19 @@ import { IFoodItem } from "../types/types";
 export const dataFood:IFoodItem[] = [
   {
     id: 1,
-    image: '/media/breakfast.png',
+    image: 'media/breakfast.png',
     title: 'Śniadanie',
     description: 'Omlet górski z warzywami. Świeże owoce i górski miód. Pachnąca kawa lub gorąca herbata.',
   },
   {
     id: 2,
-    image: '/media/lunch.png',
+    image: 'media/lunch.png',
     title: 'Obiad',
     description: 'Kasza górska z warzywami. Zupa górska. Racuchy ziemniaczane. Sałatka z świeżych warzyw i ziołami',
   },
   {
     id: 3,
-    image: '/media/dinner.png',
+    image: 'media/dinner.png',
     title: 'Kolacja',
     description: 'Pieczeń z mięsa wieprzowego duszona z cebulą i czosnkiem. Kasza jaglana mieszana z duszonymi warzywami. Mieszanka świeżych warzyw',
   },
